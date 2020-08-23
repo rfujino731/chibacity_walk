4e40c065d8b88d7e278cc8b3532de6e2b27a54cf
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-08-23 23:30:12 +0900

    modified controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-      user_path(current_user.id)
+    user_path(current_user.id)
   end
 
   def after_sign_up_path_for(resource)
-      user_path(current_user.id)
+    user_path(current_user.id)
   end
   #sign_out後のredirect先変更する。rootパスへ。rootパスはhome topを設定済み。
   def after_sign_out_path_for(resource)
-      new_session_path(resource_name)
+    new_session_path(resource_name)
   end
 
   protected
